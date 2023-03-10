ca841097897e194a1d3a95158aeae39ad3dd91cb
Thinkedou <r.enguehard@gmail.com>
2023-03-10 13:41:33 +0100

    clean up

--- js/teamsApp.js
+++ js/teamsApp.js
@@ -1,54 +1,54 @@
-console.log('teamsApp')
-
-// remplacer dynamiquement les infos de la card avec
-// les infos de l'objet oneDev si dessous :
-
-const oneDev  = {
-    'firstName' : 'Efrim',
-    'lastName'  : 'Menuk',
-    'verbatim'  : 'Behaviour-driven Self-driving Conference Calls',
-    'jobTitle'  : 'Full Stack 🚀',
-    'profilePic': 'profil01.jpg',
-    'stack':[{
-            'label':'VueJs',
-            'level':5
+export const  TEAMS =[
+    {
+        'firstName' : 'Menuk',
+        'lastName'  : 'Efrim',
+        'verbatim'  : 'Behaviour-driven Self-driving Conference Calls',
+        'jobTitle'  : 'Full Stack 🚀',
+        'profilePic': 'profil01.jpg',
+        'entry':{
+            subEntry:'yeah'
         },
-        {
-            'label':'php',
-            'level':5
-        }
-    ]
-}
+        'stack':[{
+                'label':'VueJs',
+                'level':3
+            },
+            {
+                'label':'php',
+                'level':5
+            }
+        ]
+    },
+    {
+        'firstName' : 'Elena',
+        'lastName'  : 'Stanget',
+        'verbatim'  : 'Benchmark wearable eyeballs',
+        'jobTitle'  : 'Creative Guru ',
+        'profilePic': 'profil02.jpg',
+        'stack':[{
+                'label':'AE',
+                'level':5
+            },
+            {
+                'label':'Patience',
+                'level':5
+            }
+        ]
+    },
+    {
+        'firstName' : 'Alan',
+        'lastName'  : 'Vision',
+        'verbatim'  : 'Implement vertical convergence',
+        'jobTitle'  : 'Techno-Mage',
+        'profilePic': 'profil03.jpg',
 
-
-const jobTitle = document.querySelector('.job')
-jobTitle.textContent = oneDev.jobTitle
-
-
-document.querySelector('.card-text > h2').textContent=oneDev.firstName+' '+oneDev.lastName
-
-const starLevels = document.querySelectorAll('.stat >.level')
-console.log(starLevels)
-
-starLevels[0].textContent=''
-starLevels[1].textContent=''
-
-
-// je m'occupe ici que du level de VueJs
-
-for (let i = 1; i <= oneDev.stack[0].level; i++) {
- starLevels[0].textContent=starLevels[0].textContent+'✨'
-}
-
-for (let i = 1; i <= oneDev.stack[1].level; i++) {
- starLevels[1].textContent=starLevels[1].textContent+'✨'
-}
-
-// comment faire pour attraper les deux stacks en une seule opération 
-
-
-// ! Attention le nombre d'étoiles doit bien correspondre au level
-
-// si vous êtes en avance :
-// Regardez un peu l'object destructuring ^^
-// > https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
+        'stack':[{
+                'label':'Excell',
+                'level':2
+            },
+            {
+                'label':'Quora',
+                'level':5
+            }
+        ]
+    }
+]
